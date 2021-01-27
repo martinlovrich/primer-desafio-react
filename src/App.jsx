13a928eb65e1d29db97e4fb9ec4809c8792c0493
martinlovrich <71386978+martinlovrich@users.noreply.github.com>
2021-01-27 16:02:03 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavbarComponent from './components/navbar/';
+
+const App = () => {
+  return (
+    <>
+    <NavbarComponent />
+    Estos es lo de app
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,36 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const NavbarComponent = () => {
+  return (
+    <>
+    <nav class="navbar  navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="index.html">Inicio</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Dormitorio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Baño</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Cocina</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Recomendaciones</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="" >Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+    </>
+  );
+}
+
+export default NavbarComponent;
